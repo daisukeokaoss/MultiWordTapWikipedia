fdaf900dee36215524e8ff4b4006ac5224207adc
Daisuke <daisukeokahassou@gmail.com>
2017-09-03 22:11:58 +0900

    設定のセーブロードを実装

--- Hassouseisei_Wikipedia_New/AppDelegate.swift
+++ Hassouseisei_Wikipedia_New/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var setOfWordArray = Array<OneSetWordStorage>()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
--- Hassouseisei_Wikipedia_New/FullSetWordStorageAndSettingLoadSave.swift
+++ Hassouseisei_Wikipedia_New/FullSetWordStorageAndSettingLoadSave.swift
@@ -39,8 +39,38 @@ class FullSetWordStorageAndSettingLoadSave: NSObject {
     {
         let appdelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
         
-        appdelegate
+        appdelegate.setOfWordArray = []
+        let firsttime_ = defaults.integer(forKey: "FirstTime")
+        if(firsttime_ == 0){
+            self.loadDefaultWordSet()
+        }
+        let count = defaults.integer(forKey: "WordSetCount")
         
+        for i in 0..<count{
+            let oneWordStorage = OneSetWordStorage();
+            oneWordStorage.CaptionString = defaults.string(forKey: "WordSetCaptionNo\(i)")!
+            oneWordStorage.wordSetArray  = defaults.object(forKey: "WordSetArrayNo\(i)") as! Array<String>
+            
+            oneWordStorage.Enable = defaults.bool(forKey: "WordSetEnable\(i)")
+            
+            appdelegate.setOfWordArray.append(oneWordStorage)
+        }
+        
+    }
+    
+    func saveSetting()
+    {
+        let appdelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        defaults.set(4, forKey:"FirstTime")
+        
+        defaults.set(appdelegate.setOfWordArray.count, forKey: "WordSetCount")
+        
+        for i in 0..<appdelegate.setOfWordArray.count{
+            defaults.set(appdelegate.setOfWordArray[i].CaptionString, forKey: "WordSetCaptionNo\(i)")
+            defaults.set(appdelegate.setOfWordArray[i].wordSetArray, forKey: "WordSetArrayNo\(i)")
+            defaults.set(appdelegate.setOfWordArray[i].Enable, forKey: "WordSetEnable\(i)")
+        }
     }
 
     
