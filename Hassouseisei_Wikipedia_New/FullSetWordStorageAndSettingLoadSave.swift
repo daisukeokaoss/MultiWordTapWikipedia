24d2453596f8a19e569d157f34bc5c93b18632f0
Daisuke <daisukeokahassou@gmail.com>
2017-09-03 20:26:27 +0900

    ワードセット保存作りかけ

---
+++ Hassouseisei_Wikipedia_New/FullSetWordStorageAndSettingLoadSave.swift
@@ -0,0 +1,15 @@
+//
+//  FullSetWordStorageAndSettingLoadSave.swift
+//  Hassouseisei_Wikipedia_New
+//
+//  Created by 岡大輔 on 2017/09/03.
+//  Copyright © 2017年 岡大輔. All rights reserved.
+//
+
+import UIKit
+
+class FullSetWordStorageAndSettingLoadSave: NSObject {
+
+    
+
+}
---
+++ Hassouseisei_Wikipedia_New/OneSetWordStorage.swift
@@ -0,0 +1,19 @@
+//
+//  OneSetWordStorage.swift
+//  Hassouseisei_Wikipedia_New
+//
+//  Created by 岡大輔 on 2017/09/03.
+//  Copyright © 2017年 岡大輔. All rights reserved.
+//
+
+import UIKit
+
+class OneSetWordStorage: NSObject {
+    
+    var wordSetArray = Array<String>()
+    
+    var CaptionString = ""
+    
+    var Enable = true
+
+}
