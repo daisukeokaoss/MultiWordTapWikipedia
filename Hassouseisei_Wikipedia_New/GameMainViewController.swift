778689ebec3ee0a0dab26ef7ebeabe76d30e2244
岡大輔 <daisuke_oka@nanosoftware.biz>
2017-10-23 20:53:04 +0900

    GameMainViewController追加

---
+++ Hassouseisei_Wikipedia_New/GameMainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameMainViewController.swift
+//  Hassouseisei_Wikipedia_New
+//
+//  Created by 岡大輔 on 2017/10/23.
+//  Copyright © 2017年 岡大輔. All rights reserved.
+//
+
+import UIKit
+
+class GameMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
